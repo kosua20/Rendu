4d2c192166c2735a8d6150e11838cb8c8f3a1382
Simon Rodriguez <kosua20@gmail.com>
2016-05-07 00:31:23 +0200

    Include headers for GLEW and glfw3
    GLEW is used to load OpenGL extensions (hardware dependent) at runtime.
    glfw3 is used to create a window, an OpenGL context, and handle
    interactions.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <GL/glew.h> // to load OpenGL extensions at runtime
+#include <GLFW/glfw3.h> // to set up the OpenGL context and manage window lifecycle and inputs
+
 #include <stdio.h>
 #include <iostream>
 
