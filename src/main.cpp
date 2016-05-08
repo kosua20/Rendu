fda0ebdbe9b7007460e66852a00aaea1f1061663
Simon Rodriguez <kosua20@gmail.com>
2016-05-08 21:01:08 +0200

    Added GLFW initialization and loop
    We use GLFW to create an OpenGL buffer with the right profile, and then
    create a window. We set up a rendering/inputs handling loop. At the
    end, we close the window and release the OpenGL context.
    
    
    Former-commit-id: 061c4f39fc6fe32c264e38e15d15083a98745215

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,50 @@
 #include <stdio.h>
 #include <iostream>
 
+#define INITIAL_SIZE_WIDTH 800
+#define INITIAL_SIZE_HEIGHT 600
 
 /// The main function
 
 int main () {
-	std::cout << "Hello !" << std::endl; 
+	// Initialize glfw, which will create and setup an OpenGL context.
+	if (!glfwInit()) {
+		std::cerr << "ERROR: could not start GLFW3" << std::endl;
+		return 1;
+	}
+
+	// On OS X, the correct OpenGL profile and version to use have to be explicitely defined.
+	#ifdef __APPLE__
+	glfwWindowHint (GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	#endif
+
+	// Create a window with a given size. Width and height are macros as we will need them again.
+	GLFWwindow* window = glfwCreateWindow(INITIAL_SIZE_WIDTH, INITIAL_SIZE_HEIGHT,"GL_Template", NULL, NULL);
+	if (!window) {
+		std::cerr << "ERROR: could not open window with GLFW3" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	// Bind the OpenGL context and the new window.
+	glfwMakeContextCurrent(window);
+
+	// Start the display/interaction loop.
+	while (!glfwWindowShouldClose(window)) {
+		
+		// Update the content of the window.
+		// ...
+
+		// Update events (inputs,...).
+		glfwPollEvents();
+	}
+
+	// Remove the window.
+	glfwDestroyWindow(window);
+	// Close GL context and any other GLFW resources.
+	glfwTerminate();
 	return 0;
 }
