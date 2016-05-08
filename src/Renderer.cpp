4551770939e1827c1974d68defad3ac0b60d3738
Simon Rodriguez <kosua20@gmail.com>
2016-05-08 23:02:52 +0200

    Refactoring
    Move the display logic in a renderer class (no more callbacks for now,
    but they will be back).

---
+++ src/Renderer.cpp
@@ -0,0 +1,35 @@
+
+
+#include <stdio.h>
+#include <iostream>
+
+#include "Renderer.h"
+
+Renderer::Renderer(){}
+
+Renderer::~Renderer(){}
+
+void Renderer::init(){
+	// Query the renderer identifier, and the supported OpenGL version.
+	const GLubyte* renderer = glGetString(GL_RENDERER);
+	const GLubyte* version = glGetString(GL_VERSION);
+	std::cout << "Renderer: " << renderer << std::endl;
+	std::cout << "OpenGL version supported: " << version << std::endl;
+}
+
+
+void Renderer::draw(){
+	// Set the clear color to white.
+	glClearColor(1.0f,1.0f,1.0f,0.0f);
+	// Clear the color and depth buffers.
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+}
+
+
+void Renderer::clean(){
+
+}
+
+
+
---
+++ src/Renderer.h
@@ -0,0 +1,26 @@
+#include <GLFW/glfw3.h> // to set up the OpenGL context and manage window lifecycle and inputs
+
+class Renderer {
+
+public:
+
+Renderer();
+
+~Renderer();
+
+/// Init function
+void init();
+
+/// Draw function
+void draw();
+
+/// Clean function
+void clean();
+
+
+private:
+
+
+};
+
+
