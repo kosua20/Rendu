abd6210ec4d384585c13a1867e395d3327eca420
Simon Rodriguez <kosua20@gmail.com>
2016-05-15 01:08:19 +0200

    Camera receive key inputs
    The camera is notified of each press and release of a directional key.
    A reset function is also added.

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -5,16 +5,23 @@
 #include "Camera.h"
 
 Camera::Camera(){
-	_eye = glm::vec3(0.0,0.0,1.0);
-	_center = glm::vec3(0.0,0.0,0.0);
-	_up = glm::vec3(0.0,1.0,0.0);
-	_view = glm::lookAt(_eye, _center, _up);
+	reset();
 } 
 
 Camera::~Camera(){}
 
+void Camera::reset(){
+	_eye = glm::vec3(0.0,0.0,1.0);
+	_center = glm::vec3(0.0,0.0,0.0);
+	_up = glm::vec3(0.0,1.0,0.0);
+	_view = glm::lookAt(_eye, _center, _up);
+}
 
 void Camera::update(float elapsedTime){
+}
+
+void Camera::registerMove(int direction, bool flag){
+	std::cout << (flag ? "Begin " : "End " ) << direction << std::endl;
 }
 
 
