1fe67d3e166e852f42cb6f767eee9260b1c7959d
Simon Rodriguez <kosua20@gmail.com>
2016-05-15 01:27:55 +0200

    Add movements in the horizontal plane.
    For each key, we keep track of its state in an array. At update time,
    we move the characteristic points of the camera (eye, center) using
    these states, and update the up and right vectors.

--- src/Camera.h
+++ src/Camera.h
@@ -11,18 +11,27 @@ public:
 
 	~Camera();
 
+	/// Reset the position of the camera.
 	void reset();
 
+	/// Update the view matrix.
 	void update(float elapsedTime);
 
+	/// Register a pressed or release movement key.
 	void registerMove(int direction, bool flag);
-	
+
+	/// The view matrix.
 	glm::mat4 _view;
 
 private:
 	glm::vec3 _eye;
 	glm::vec3 _center;
 	glm::vec3 _up;
+	glm::vec3 _right;
+
+	bool _keys[4];
+
+	float _speed;
 
 };
 
