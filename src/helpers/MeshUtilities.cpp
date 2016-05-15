fe9c5a1aab9b119d17b4e22c18582d97a98d88f7
Simon Rodriguez <kosua20@gmail.com>
2016-05-16 01:13:24 +0200

    Load an obj from disk
    In a new helper class, a mesh structure is defined (containing
    positions, normals, texture coordinates and indices). A function to
    load obj files from disk with different modes, along with a function to
    adjust the position and size of such a mesh, are defined.
    
    
    Former-commit-id: 37070da85b3cdc396bee2f44e032a1667656c8a0

---
+++ src/helpers/MeshUtilities.cpp
@@ -0,0 +1,16 @@
+#include "MeshUtilities.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstddef>
+#include <map>
+
+using namespace std;
+
+void loadObj(const std::string & filename, mesh_t & mesh, LoadMode mode){
+	
+}
+
+void centerAndUnitMesh(mesh_t & mesh){
+
+}
