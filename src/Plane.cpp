e2132c8511b82df4d8e70de5359ec62fe8fcd09c
Simon Rodriguez <kosua20@gmail.com>
2016-08-02 22:27:56 +0200

    Plane is now loaded from an obj
    and support full lighting shader, as the other objects.
    
    
    Former-commit-id: 6c7c0799c445c53ef1b3b8468ce6a0743c4563b7

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -5,6 +5,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 #include "helpers/ProgramUtilities.h"
+#include "helpers/MeshUtilities.h"
 
 #include "Plane.h"
 
@@ -15,39 +16,43 @@ Plane::~Plane(){}
 void Plane::init(GLuint shadowMapTextureId){
 	
 	// Load the shaders
-	_programDepthId = createGLProgram("ressources/shaders/plane_depth.vert","ressources/shaders/plane_depth.frag");
+	_programDepthId = createGLProgram("ressources/shaders/object_depth.vert","ressources/shaders/object_depth.frag");
 	_programId = createGLProgram("ressources/shaders/plane.vert","ressources/shaders/plane.frag");
 
 	// Load geometry.
-	std::vector<float> cubeVertices{ -1.0, 0.0,  -1.0,
-		1.0, 0.0,  -1.0,
-		-1.0, 0.0, 1.0,
-		1.0, 0.0, 1.0
-	};
+	mesh_t mesh;
+	loadObj("ressources/plane.obj",mesh,Indexed);
+	centerAndUnitMesh(mesh);
+	computeTangentsAndBinormals(mesh);
 	
-	std::vector<float> cubeNormals{ 0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0,
-		0.0, 1.0, 0.0
-	};
-
-	// Array to store the indices of the vertices to use.
-	std::vector<unsigned int> cubeIndices{ 0, 2, 1, 1, 2, 3 };
-	
-	_count = cubeIndices.size();
+	_count = mesh.indices.size();
 
 	// Create an array buffer to host the geometry data.
 	GLuint vbo = 0;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	// Upload the data to the Array buffer.
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * cubeVertices.size(), &(cubeVertices[0]), GL_STATIC_DRAW);
-
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * mesh.positions.size() * 3, &(mesh.positions[0]), GL_STATIC_DRAW);
+	
 	GLuint vbo_nor = 0;
 	glGenBuffers(1, &vbo_nor);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_nor);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * cubeNormals.size() * 3, &(cubeNormals[0]), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * mesh.normals.size() * 3, &(mesh.normals[0]), GL_STATIC_DRAW);
 	
+	GLuint vbo_uv = 0;
+	glGenBuffers(1, &vbo_uv);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_uv);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * mesh.texcoords.size() * 2, &(mesh.texcoords[0]), GL_STATIC_DRAW);
+	
+	GLuint vbo_tan = 0;
+	glGenBuffers(1, &vbo_tan);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_tan);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * mesh.tangents.size() * 3, &(mesh.tangents[0]), GL_STATIC_DRAW);
+	
+	GLuint vbo_binor = 0;
+	glGenBuffers(1, &vbo_binor);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_binor);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * mesh.binormals.size() * 3, &(mesh.binormals[0]), GL_STATIC_DRAW);
 	
 	// Generate a vertex array (useful when we add other attributes to the geometry).
 	_vao = 0;
@@ -57,27 +62,53 @@ void Plane::init(GLuint shadowMapTextureId){
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-	// The second attribute will be the vertices normals.
+	
+	// The second attribute will be the normals.
 	glEnableVertexAttribArray(1);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_nor);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-
+	
+	// The third attribute will be the uvs.
+	glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_uv);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+	
+	// The fourth attribute will be the tangents.
+	glEnableVertexAttribArray(3);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_tan);
+	glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+	
+	// The fifth attribute will be the binormals.
+	glEnableVertexAttribArray(4);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_binor);
+	glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 	
 	// We load the indices data
 	glGenBuffers(1, &_ebo);
- 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ebo);
- 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * cubeIndices.size(), &(cubeIndices[0]), GL_STATIC_DRAW);
-
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * mesh.indices.size(), &(mesh.indices[0]), GL_STATIC_DRAW);
+	
 	glBindVertexArray(0);
 	
 	// Get a binding point for the light in Uniform buffer.
 	_lightUniformId = glGetUniformBlockIndex(_programId, "Light");
 	
+	// Load and upload the textures.
+	_texColor = loadTexture("ressources/plane_texture_color.png", _programId, 0,  "textureColor", true);
+	
+	_texNormal = loadTexture("ressources/plane_texture_normal.png", _programId, 1, "textureNormal");
+	
+	_texEffects = loadTexture("ressources/plane_texture_depthmap.png", _programId, 2, "textureEffects");
+	
+	_texCubeMap = loadTextureCubeMap("ressources/cubemap/cubemap", _programId, 3, "textureCubeMap", true);
+	
+	_texCubeMapSmall = loadTextureCubeMap("ressources/cubemap/cubemap_diff", _programId, 4, "textureCubeMapSmall", true);
+	
 	// Load the shadowMap texture
 	_shadowMapId = shadowMapTextureId;
 	glBindTexture(GL_TEXTURE_2D, _shadowMapId);
 	GLuint texUniID = glGetUniformLocation(_programId, "shadowMap");
-	glUniform1i(texUniID, 0);
+	glUniform1i(texUniID, 5);
 	
 	checkGLError();
 	
@@ -93,7 +124,8 @@ void Plane::draw(float elapsed, const glm::mat4& view, const glm::mat4& projecti
 	
 	// Compute the normal matrix
 	glm::mat3 normalMatrix = glm::transpose(glm::inverse(glm::mat3(MV)));
-	
+	// Compute the inverse view matrix
+	glm::mat4 invView = glm::inverse(view);
 	// Select the program (and shaders).
 	glUseProgram(_programId);
 
@@ -109,13 +141,28 @@ void Plane::draw(float elapsed, const glm::mat4& view, const glm::mat4& projecti
 	// Upload the normal matrix.
 	GLuint normalMatrixID  = glGetUniformLocation(_programId, "normalMatrix");
 	glUniformMatrix3fv(normalMatrixID, 1, GL_FALSE, &normalMatrix[0][0]);
+	// Upload the inverse view matrix.
+	GLuint invVID  = glGetUniformLocation(_programId, "inverseV");
+	glUniformMatrix4fv(invVID, 1, GL_FALSE, &invView[0][0]);
+	
+	// Bind the textures.
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, _texColor);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, _texNormal);
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_2D, _texEffects);
+	glActiveTexture(GL_TEXTURE3);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, _texCubeMap);
+	glActiveTexture(GL_TEXTURE4);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, _texCubeMapSmall);
 	
 	// Upload the light MVP matrix.
 	GLuint lmvpID  = glGetUniformLocation(_programId, "lightMVP");
 	glUniformMatrix4fv(lmvpID, 1, GL_FALSE, &_lightMVP[0][0]);
 	
 	// Bind the shadowMap texture.
-	glActiveTexture(GL_TEXTURE0);
+	glActiveTexture(GL_TEXTURE5);
 	glBindTexture(GL_TEXTURE_2D, _shadowMapId);
 	
 	// Select the geometry.
