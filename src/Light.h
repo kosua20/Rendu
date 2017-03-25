6bd675fb83ff005ff6e65b8b4babdd27702564dc
Simon Rodriguez <kosua20@gmail.com>
2017-03-25 22:59:19 +0100

    Re-organize the Light objects
    Create a Light subclass called DirectionalLight, which use a fullscreen
    quad to render the light contribution in a deferred rendering pipeline.
    
    
    Former-commit-id: c22a59debf464c7c1d8208adf630c8560f4f6020

--- src/Light.h
+++ src/Light.h
@@ -1,53 +1,56 @@
 #ifndef Light_h
 #define Light_h
-
+#include <gl3w/gl3w.h>
+#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
-
-
-enum class LightType {
-	Directional, Point,
-};
-
-struct LightInternal
-{
-	glm::vec4 _p;
-	glm::vec4 _Ia;
-	glm::vec4 _Id;
-	glm::vec4 _Is;
-	float _shininess;
-	
-	
-	LightInternal() { }
-	
-	LightInternal(const glm::vec4& p, const glm::vec4& Ia, const glm::vec4& Id, const glm::vec4& Is, const float shininess) : _p(p), _Ia(Ia), _Id(Id), _Is(Is), _shininess(shininess){ }
-};
+#include <glm/gtc/matrix_transform.hpp>
+#include <map>
+#include <string>
 
 
 class Light {
 
 public:
-
-	Light();
 	
-	Light(const glm::vec4& local, const glm::vec4& Ia, const glm::vec4& Id, const glm::vec4& Is, const float shininess, const glm::mat4 projectionMatrix);
-	
-	~Light();
+	Light(const glm::vec3& worldPosition, const glm::vec3& color, const glm::mat4& projection = glm::mat4(1.0f));
 	
 	void update(float time, const glm::mat4& camViewMatrix);
 	
-	const LightInternal* getStruct(){ return &_lightStruct; }
+	virtual void init(std::map<std::string, GLuint> textureIds)=0;
+	
+	virtual void draw(const glm::vec2& invScreenSize, const glm::mat4& viewMatrix, const glm::mat4& projectionMatrix) =0;
 	
 	glm::mat4 _mvp;
+	glm::vec3 _color;
 	
-private:
+protected:
 	
-	LightInternal _lightStruct;
 	glm::mat4 _projectionMatrix;
 	glm::mat4 _viewMatrix;
-	
 	glm::vec3 _local;
-	LightType _type;
 	
 };
 
+
+inline Light::Light(const glm::vec3& worldPosition, const glm::vec3& color, const glm::mat4& projection){
+	
+	_local = worldPosition;
+	_color = color;
+	_projectionMatrix = projection;
+	_viewMatrix = glm::lookAt(_local, glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.0f,1.0f,0.0f));
+	_mvp = _projectionMatrix * _viewMatrix;
+	
+}
+
+
+inline void Light::update(float time, const glm::mat4& camViewMatrix){
+	
+	_local = glm::vec3(2.0f,(1.5f + sin(0.5*time)),2.0f);
+	_viewMatrix = glm::lookAt(_local, glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.0f,1.0f,0.0f));
+	_mvp = _projectionMatrix * _viewMatrix;
+	
+}
+
 #endif
+
+
