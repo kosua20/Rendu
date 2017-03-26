6e71d88daade7549ac914e45676b0eb901c934e7
Simon Rodriguez <kosua20@gmail.com>
2017-03-27 00:00:29 +0200

    Integrate shadow maps inside DirectionalLights
    Each directional light is responsible for the generation and processing
    of its shadow map. The shadow map is then used when additively
    rendering the light.
    
    
    Former-commit-id: a6f16a946162903e2cbe5d05b027d5a3f5eb6703

--- src/lights/DirectionalLight.h
+++ src/lights/DirectionalLight.h
@@ -2,6 +2,7 @@
 #define DirectionalLight_h
 #include "Light.h"
 #include "../ScreenQuad.h"
+#include "../Framebuffer.h"
 
 class DirectionalLight : public Light {
 
@@ -9,13 +10,23 @@ public:
 	
 	DirectionalLight(const glm::vec3& worldPosition, const glm::vec3& color, const glm::mat4& projection = glm::mat4(1.0f));
 	
-	void init(std::map<std::string, GLuint> textureIds);
+	void init(const std::map<std::string, GLuint>& textureIds);
 	
 	void draw(const glm::vec2& invScreenSize, const glm::mat4& viewMatrix, const glm::mat4& projectionMatrix);
-		
+	
+	void bind();
+	
+	void blurAndUnbind();
+	
+	void clean();
+	
 private:
 	
 	ScreenQuad _screenquad;
+	ScreenQuad _blurScreen;
+	std::shared_ptr<Framebuffer> _shadowPass;
+	std::shared_ptr<Framebuffer> _blurPass;
+	
 };
 
 #endif
