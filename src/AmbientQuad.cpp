9fcb7db936c9f00060715a788e6785edafeff4db
Simon Rodriguez <kosua20@gmail.com>
2017-03-30 22:23:16 +0200

    Modify Ambient quad to setup SSAO pass
    The SSAO pass will require the normals and depth, along with a random
    noise texture and a series of samples (set up later).
    
    
    Former-commit-id: 5f8902897102e01271031eaa357ab87a72d03284

--- src/AmbientQuad.cpp
+++ src/AmbientQuad.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "helpers/ProgramUtilities.h"
+#include "helpers/GenerationUtilities.h"
 
 #include "AmbientQuad.h"
 
@@ -10,14 +11,24 @@ AmbientQuad::AmbientQuad(){}
 
 AmbientQuad::~AmbientQuad(){}
 
-void AmbientQuad::init(std::map<std::string, GLuint> textureIds, const std::string & shaderRoot){
+void AmbientQuad::init(std::map<std::string, GLuint> textureIds){
 	
-	ScreenQuad::init(textureIds, shaderRoot);
-	
+	// Ambient pass: needs the albedo, the normals and the AO result
+	std::map<std::string, GLuint> finalTextures = { {"albedoTexture", textureIds["albedoTexture"]}, {"normalTexture", textureIds["normalTexture"]}, {"ssaoTexture", textureIds["ssaoTexture"]}};
+	ScreenQuad::init(finalTextures, "ressources/shaders/gbuffer/ambient");
 	_texCubeMapSmall = loadTextureCubeMap("ressources/cubemap/cubemap_diff",
 										  _programId, (GLuint)_textureIds.size(),
 										  "textureCubeMapSmall", true);
 	
+	// Setup SSAO data, get back noise texture id, add it to the gbuffer outputs.
+	GLuint noiseTextureID = setupSSAO();
+	std::map<std::string, GLuint> ssaoTextures = { {"depthTexture", textureIds["depthTexture"]}, {"normalTexture", textureIds["normalTexture"]}, {"noiseTexture",noiseTextureID}};
+	_ssaoScreen.init(ssaoTextures, "ressources/shaders/gbuffer/ssao");
+	
+}
+
+GLuint AmbientQuad::setupSSAO(){
+	return 0;
 }
 
 void AmbientQuad::draw(const glm::vec2& invScreenSize, const glm::mat4& viewMatrix, const glm::mat4& projectionMatrix) {
@@ -29,14 +40,29 @@ void AmbientQuad::draw(const glm::vec2& invScreenSize, const glm::mat4& viewMatr
 	GLuint invVID  = glGetUniformLocation(_programId, "inverseV");
 	glUniformMatrix4fv(invVID, 1, GL_FALSE, &invView[0][0]);
 	
-	
 	glActiveTexture(GL_TEXTURE0 + (unsigned int)_textureIds.size());
 	glBindTexture(GL_TEXTURE_CUBE_MAP, _texCubeMapSmall);
 	
 	ScreenQuad::draw(invScreenSize);
 }
 
+void AmbientQuad::drawSSAO(const glm::vec2& invScreenSize, const glm::mat4& viewMatrix, const glm::mat4& projectionMatrix) {
+	
+	// Projection parameter for position reconstruction.
+	glm::vec4 projectionVector = glm::vec4(projectionMatrix[0][0], projectionMatrix[1][1], projectionMatrix[2][2], projectionMatrix[3][2]);
+	
+	glUseProgram(_ssaoScreen.program());
+	
+	GLuint projId = glGetUniformLocation(_ssaoScreen.program(), "projectionMatrix");
+	glUniform4fv(projId, 1, &(projectionVector[0]));
+	
+	_ssaoScreen.draw(invScreenSize);
+	
+}
+
+
 
 void AmbientQuad::clean(){
 	ScreenQuad::clean();
+	_ssaoScreen.clean();
 }
