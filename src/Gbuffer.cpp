f396c3b2912e8c4999168b0ad4de8d9cd208d555
Simon Rodriguez <kosua20@gmail.com>
2017-04-02 15:28:14 +0200

    We now use float format render targets for colors and lighting.
    We can thus accumulate all light and keep the real value, avoiding
    [0,1] clamping. We can also handle emissive materials (with colors
    components > 1).
    
    
    Former-commit-id: 5e6ca8c007c24579a831fe732e777969f63afa56

--- src/Gbuffer.cpp
+++ src/Gbuffer.cpp
@@ -19,7 +19,7 @@ Gbuffer::Gbuffer(int width, int height) {
 	
 	glGenTextures(1, &albedoId);
 	glBindTexture(GL_TEXTURE_2D, albedoId);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width , _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, _width , _height, 0, GL_RGBA, GL_FLOAT, 0);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
