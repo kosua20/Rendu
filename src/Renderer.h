76201bbcf11ba6e40353bf68a4abd22d8fdd758c
Simon Rodriguez <kosua20@gmail.com>
2017-04-02 15:25:28 +0200

    Add a tonemapping pass for future HDR.
    After light accumulation and before applying antialiasing, we will
    perform a tonemapping pass. Once HDR rendering will be on, it will
    allows us to convert HDR values to low-range one at then end. For now,
    it simply displays areas where the lighting is > 1 in red. As we are
    still in a LDR pipeline, there is none for now.

--- src/Renderer.h
+++ src/Renderer.h
@@ -62,10 +62,12 @@ private:
 	std::shared_ptr<Framebuffer> _ssaoFramebuffer;
 	std::shared_ptr<Framebuffer> _ssaoBlurFramebuffer;
 	std::shared_ptr<Framebuffer> _sceneFramebuffer;
+	std::shared_ptr<Framebuffer> _toneMappingFramebuffer;
 	std::shared_ptr<Framebuffer> _fxaaFramebuffer;
 
 	AmbientQuad _ambientScreen;
 	ScreenQuad _ssaoBlurScreen;
+	ScreenQuad _toneMappingScreen;
 	ScreenQuad _fxaaScreen;
 	ScreenQuad _finalScreen;
 
