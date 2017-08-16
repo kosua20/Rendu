1e24e0e075230f39284c782cc8f8af99bad4bc20
Simon Rodriguez <kosua20@gmail.com>
2017-08-17 00:27:53 +0200

    Query buttons and axis updates
    and display them in the console for now.
    
    
    Former-commit-id: b2cf98dfc69d7b8fe7120e1bae9ad303a7cf3932

--- src/camera/Joystick.cpp
+++ src/camera/Joystick.cpp
@@ -12,6 +12,8 @@ Joystick::Joystick(glm::vec3 & eye, glm::vec3 & center, glm::vec3 & up, glm::vec
 	_angularSpeed = 4.0f;
 	_id = -1;
 	
+	configure();
+	
 	reset();
 } 
 
@@ -19,6 +21,11 @@ Joystick::~Joystick(){}
 
 void Joystick::activate(int id){
 	_id = id;
+	// Get axes and buttons references and count from GLFW
+	_axes = glfwGetJoystickAxes(_id, &_axisCount);
+	_buttons = glfwGetJoystickButtons(_id, &_buttonsCount);
+	
+	reset();
 }
 
 void Joystick::deactivate(){
@@ -30,6 +37,28 @@ void Joystick::reset(){
 }
 
 void Joystick::update(float elapsedTime){
+	// Update buttons flags.
+	_axes = glfwGetJoystickAxes(_id, &_axisCount);
+	_buttons = glfwGetJoystickButtons(_id, &_buttonsCount);
+	
+	// Handle buttons
+	// ...
+	std::cout << "Buttons:";
+	for(size_t i = 0; i < _buttonsCount; ++i){
+		std::cout << " " << i << ":" << int(_buttons[i]);
+	}
+	std::cout << std::endl;
+	
+	// Handle axis
+	// ...
+	std::cout << "Axis:";
+	for(size_t i = 0; i < _axisCount; ++i){
+		std::cout << " " << i << ":" << _axes[i];
+	}
+	std::cout << std::endl;
+}
+
+void Joystick::configure(){
 	
 }
 
