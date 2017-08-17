6d9ae93bf29450256e731d97fd3ac6f2d39a3f92
Simon Rodriguez <kosua20@gmail.com>
2017-08-17 21:17:41 +0200

    Add a Controller mapping
    Stored in an external file and read at run time.

--- src/camera/Joystick.cpp
+++ src/camera/Joystick.cpp
@@ -1,9 +1,9 @@
-#include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <iostream>
-#include <fstream>
-
+#include <sstream>
 #include <glm/gtc/matrix_transform.hpp>
+#include "../helpers/ResourcesManager.h"
+#include <GLFW/glfw3.h>
 
 #include "Joystick.h"
 
@@ -11,8 +11,6 @@ Joystick::Joystick(glm::vec3 & eye, glm::vec3 & center, glm::vec3 & up, glm::vec
 	_speed = 1.0f;
 	_angularSpeed = 4.0f;
 	_id = -1;
-	
-	configure();
 	
 	reset();
 } 
@@ -24,7 +22,7 @@ void Joystick::activate(int id){
 	// Get axes and buttons references and count from GLFW
 	_axes = glfwGetJoystickAxes(_id, &_axisCount);
 	_buttons = glfwGetJoystickButtons(_id, &_buttonsCount);
-	
+	configure();
 	reset();
 }
 
@@ -35,7 +33,7 @@ void Joystick::deactivate(){
 void Joystick::reset(){
 	_recentPress[SPEED_UP] = false;
 	_recentPress[SPEED_DOWN] = false;
-	configure();
+	
 }
 
 void Joystick::update(float elapsedTime){
@@ -127,18 +125,64 @@ void Joystick::configure(){
 	// L horizontal, L vertical, R horizontal, R vertical, L2, R2
 	// PS4 codes: from 0 to 17
 	// Square, Cross, Circle, Triangle, L1, R1, L2, R2, Share, Option, L3, R3, PS button, Touchpad, Up, Right, Down, Left
+	const std::string settingsContent = Resources::manager().getTextFile("Controller.map");
+	// If no mapping found, disable the controller.
+	if(settingsContent.empty()){
+		std::cerr << "No settings found for the controller." << std::endl;
+		_id = -1;
+		return;
+	}
+	// Parse the config file and update the map with it.
 	_codes.clear();
-	_codes[MOVE_FORWARD] = 1;
-	_codes[MOVE_LATERAL] = 0;
-	_codes[LOOK_VERTICAL] = 3;
-	_codes[LOOK_LATERAL] = 2;
-	_codes[MOVE_UP] = 4;
-	_codes[MOVE_DOWN] = 5;
-	_codes[RESET_ALL] = 2;
-	_codes[RESET_CENTER] = 5;
-	_codes[RESET_ORIENTATION] = 4;
-	_codes[SPEED_UP] = 14;
-	_codes[SPEED_DOWN] = 16;
+	
+	std::istringstream lines(settingsContent);
+	std::string line;
+	while(std::getline(lines,line)){
+		// Split line at colon.
+		const size_t separatorPos = line.find_first_of(":");
+		// If no colon, skip line.
+		if(separatorPos == std::string::npos){
+			continue;
+		}
+		const std::string key = trim(trim(line.substr(0, separatorPos), " "), "\t");
+		const int val = std::stoi(line.substr(separatorPos + 1));
+		if(key == "MOVE_FORWARD") {
+			_codes[MOVE_FORWARD] = val;
+		} else if(key == "MOVE_LATERAL") {
+			_codes[MOVE_LATERAL] = val;
+		} else if(key == "LOOK_VERTICAL") {
+			_codes[LOOK_VERTICAL] = val;
+		} else if(key == "LOOK_LATERAL") {
+			_codes[LOOK_LATERAL] = val;
+		} else if(key == "MOVE_UP") {
+			_codes[MOVE_UP] = val;
+		} else if(key == "MOVE_DOWN") {
+			_codes[MOVE_DOWN] = val;
+		} else if(key == "RESET_ALL") {
+			_codes[RESET_ALL] = val;
+		} else if(key == "RESET_CENTER") {
+			_codes[RESET_CENTER] = val;
+		} else if(key == "RESET_ORIENTATION") {
+			_codes[RESET_ORIENTATION] = val;
+		} else if(key == "SPEED_UP") {
+			_codes[SPEED_UP] = val;
+		} else if(key == "SPEED_DOWN") {
+			_codes[SPEED_DOWN] = val;
+		} else {
+			// Unknown key, ignore.
+			std::cerr << "Unknown key: " << key << "." << std::endl;
+		}
+	}
 
+}
+
+
+std::string Joystick::trim(const std::string & str, const std::string & del){
+	const size_t firstNotDel = str.find_first_not_of(del);
+	if(firstNotDel == std::string::npos){
+		return "";
+	}
+	const size_t lastNotDel = str.find_last_not_of(del);
+	return str.substr(firstNotDel, lastNotDel - firstNotDel + 1);
 }
 
