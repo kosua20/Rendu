878502b7cc5e6d7800b5762c452171e1cacfd954
Simon Rodriguez <kosua20@gmail.com>
2017-09-17 12:05:01 +0200

    Fix directional light position.
    Former-commit-id: 44e621440a7ea3fab7b02aac5c8688791ccb2c5b

--- src/scenes/SphereScene.h
+++ src/scenes/SphereScene.h
@@ -19,7 +19,7 @@ public:
 void SphereScene::init(){
 	
 	// Create directional light.
-	directionalLights.emplace_back(glm::vec3(0.0f), 1.2f*glm::vec3(1.0f,1.0f, 0.92f), glm::ortho(-0.75f,0.75f,-0.75f,0.75f,1.0f,6.0f));
+	directionalLights.emplace_back(glm::vec3(-2.0f, 1.5f, 0.0f), 5.0f*glm::vec3(1.0f,1.0f, 0.92f), glm::ortho(-0.75f,0.75f,-0.75f,0.75f,1.0f,6.0f));
 	
 	pointLights.emplace_back( glm::vec3(0.5f,-0.1f,0.5f), 3.0f*glm::vec3(0.2f,0.8f,1.2f), 0.9f);
 	
@@ -37,6 +37,7 @@ void SphereScene::init(){
 void SphereScene::update(double timer, double elapsedTime){
 	const glm::mat4 model = glm::rotate(glm::scale(glm::mat4(1.0f),glm::vec3(0.35f)), 0.2f*float(timer), glm::vec3(0.0f,1.0f,0.0f));
 	objects[0].update(model);
+	
 }
 
 #endif
