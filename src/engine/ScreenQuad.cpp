0a7985c5f7692bd63e9859768407ef2eb43e55ba
Simon Rodriguez <kosua20@gmail.com>
2018-09-01 00:45:29 +0200

    Remove the ScreenQuad instance methods
    We only need a static method to bind textures and draw the geometry.
    
    
    Former-commit-id: bdc15c4abbe7b70c7af82b90071fe7d429d98e11

--- src/engine/ScreenQuad.cpp
+++ src/engine/ScreenQuad.cpp
@@ -4,134 +4,36 @@
 
 GLuint ScreenQuad::_vao = 0;
 
-ScreenQuad::ScreenQuad(){}
 
-ScreenQuad::~ScreenQuad(){}
-
-void ScreenQuad::init(const std::string & shaderRoot){
-	
-	// Load the shaders
-	_program = Resources::manager().getProgram(shaderRoot, "passthrough", shaderRoot);
-	
-	// Load geometry.
-	loadGeometry();
-	
-	_program->registerTexture("screenTexture", 0);
-	
-	checkGLError();
-}
-
-void ScreenQuad::init(GLuint textureId, const std::string & shaderRoot){
-	
-	// Load the shaders
-	_program = Resources::manager().getProgram(shaderRoot, "passthrough", shaderRoot);
-
-	// Load geometry.
-	loadGeometry();
-	
-	// Link the texture of the framebuffer for this program.
-	_textureIds.clear();
-	_textureIds.push_back(textureId);
-	_program->registerTexture("screenTexture", 0);
-	
-	checkGLError();
-	
-}
-
-void ScreenQuad::init(std::map<std::string, GLuint> textureIds, const std::string & shaderRoot){
-	
-	// Load the shaders
-	_program = Resources::manager().getProgram(shaderRoot, "passthrough", shaderRoot);
-	
-	loadGeometry();
-	
-	// Link the texture of the framebuffer for this program.
-	GLint currentTextureSlot = 0;
-	_textureIds.clear();
-	for(auto& texture : textureIds){
-		_textureIds.push_back(texture.second);
-		_program->registerTexture(texture.first, currentTextureSlot);
-		currentTextureSlot += 1;
-	}
-
-	checkGLError();
-	
-}
-
-void ScreenQuad::loadGeometry(){
+void ScreenQuad::draw() {
 	if(_vao == 0){
 		// Generate an empty VAO (imposed by the OpenGL spec).
 		glGenVertexArrays (1, &_vao);
 		glBindVertexArray(_vao);
 		glBindVertexArray(0);
 	}
-}
-
-void ScreenQuad::draw() const {
-	
-	// Select the program (and shaders).
-	glUseProgram(_program->id());
-	
-	// Active screen texture.
-	for(GLuint i = 0;i < _textureIds.size(); ++i){
-		glActiveTexture(GL_TEXTURE0 + i);
-		glBindTexture(GL_TEXTURE_2D, _textureIds[i]);
-	}
-	
 	// Draw with an empty VAO (mandatory)
 	glBindVertexArray(_vao);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
-	
 	glBindVertexArray(0);
 	glUseProgram(0);
 }
 
-void ScreenQuad::draw(const glm::vec2& invScreenSize) const {
-	
-	// Select the program (and shaders).
-	glUseProgram(_program->id());
-	
-	// Inverse screen size uniform.
-	glUniform2fv(_program->uniform("inverseScreenSize"), 1, &(invScreenSize[0]));
-	
-	draw();
-	
-}
-
-void ScreenQuad::draw(const GLuint textureId) const {
-	
-	// Select the program (and shaders).
-	glUseProgram(_program->id());
-	
-	// Override stored textures.
+void ScreenQuad::draw(const GLuint textureId) {
+	// Active screen texture.
 	glActiveTexture(GL_TEXTURE0 );
 	glBindTexture(GL_TEXTURE_2D, textureId);
-	
-	// Draw with an empty VAO (mandatory)
-	glBindVertexArray(_vao);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	
-	glBindVertexArray(0);
-	glUseProgram(0);
+	draw();
 }
 
-void ScreenQuad::draw(const GLuint textureId, const glm::vec2& invScreenSize) const {
-	
-	// Select the program (and shaders).
-	glUseProgram(_program->id());
-	
-	// Inverse screen size uniform.
-	glUniform2fv(_program->uniform("inverseScreenSize"), 1, &(invScreenSize[0]));
-	
-	draw(textureId);
+void ScreenQuad::draw(const std::vector<GLuint> & textures) {
+	// Active screen textures.
+	for(GLuint i = 0; i < textures.size(); ++i){
+		glActiveTexture(GL_TEXTURE0 + i);
+		glBindTexture(GL_TEXTURE_2D, textures[i]);
+	}
+	draw();
 }
 
-
-
-
-void ScreenQuad::clean() const {
-	// The vao is shared.
-	//glDeleteVertexArrays(1, &_vao);
-}
 
 
--- src/engine/ScreenQuad.hpp
+++ src/engine/ScreenQuad.hpp
@@ -11,39 +11,17 @@ class ScreenQuad {
 
 public:
 
-	ScreenQuad();
-
-	~ScreenQuad();
-
-	/// Init function
-	void init(const std::string & shaderRoot);
-	
-	void init(GLuint textureId, const std::string & shaderRoot);
-	
-	void init(std::map<std::string, GLuint> textureIds, const std::string & shaderRoot);
-
 	/// Draw function,
-	void draw() const;
+	static void draw();
 	
-	void draw(const glm::vec2& invScreenSize) const;
+	static void draw(GLuint textureId);
 	
-	void draw(GLuint textureId) const;
+	static void draw(const std::vector<GLuint> & textureIds);
 	
-	void draw(GLuint textureId, const glm::vec2& invScreenSize) const;
-
-	/// Clean function
-	void clean() const;
-
-	const std::shared_ptr<ProgramInfos> program() const { return _program; }
 	
-	std::shared_ptr<ProgramInfos> program() { return _program; }
+private:
 	
-protected:
-	
-	static void loadGeometry();
-	
-	std::shared_ptr<ProgramInfos> _program;
-	std::vector<GLuint> _textureIds;
+	ScreenQuad();
 	
 	static GLuint _vao;
 };
