bad8fa8ae7ed4e0298acd5c5404a4ebdac12e544
Simon Rodriguez <kosua20@gmail.com>
2018-09-03 23:46:50 +0200

    Documentation: add support for shaders.
    Add a python script that generate a C++ class for each shader, in a GLSL namespace.
    
    
    Former-commit-id: 6696a11e49abaa836e842543ca13a49cc8308f6b

---
+++ docs/theme/process-glsl.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+# If no documentation is generated, make sure that this file is executable.
+import sys
+import os
+
+def printout(text):
+	sys.stdout.write(text)
+
+if len(sys.argv) != 2:
+	sys.exit(1)
+
+# Get the file infos.
+filePath = sys.argv[1]
+fileSubpath, fileExt = os.path.splitext(filePath)
+fileDir, fileName = os.path.split(fileSubpath)
+fileExt = fileExt
+fileName = fileName
+
+# Get the file content.
+fileHandle = open(filePath)
+fileLines = fileHandle.readlines()
+fileHandle.close()
+
+# Doxygen infos.
+doxyNamespace = "GLSL"
+doxyGroup = "Shaders"
+doxySubnamespace = fileExt.lower()[1:].capitalize()
+doxyClass = fileName.capitalize().replace("-", "_")
+
+# Main comment block
+# Namespace and group
+printout("/** \\namespace " + doxyNamespace + "::" + doxySubnamespace + "\n")
+printout("  * \\addtogroup " + doxyGroup + "\n @{ */\n")
+# C++ namespaces
+printout("namespace " + doxyNamespace + "{\n")
+printout("namespace " + doxySubnamespace + "{\n")
+# Class
+printout("/** \\class " + doxyClass + " */\n")
+printout("public class " + doxyClass + " {" + "\n")
+printout("public:" + "\n")
+# Content of the shader
+for line in fileLines:
+	printout(line)
+# Close the class, the namespaces, the group.
+printout("}" + "\n}\n}\n/**@}*/\n")
+
+sys.exit(0)
