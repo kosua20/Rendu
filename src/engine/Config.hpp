dfaf47319de6bda083093f996e4e75c147a5790a
Simon Rodriguez <kosua20@gmail.com>
2018-09-03 08:22:02 +0200

    Config: documentation.
    Former-commit-id: c3b9b19509e7ca6339d5eea62568bf639ee0a859

--- src/engine/Config.hpp
+++ src/engine/Config.hpp
@@ -11,45 +11,87 @@
 #include "Common.hpp"
 #include <map>
 
+/**
+ \class Config
+ \brief Contains configurable elements as attributes, populated from the command line, a configuration file or default values.
+ */
 class Config {
 public:
 	
+	/** Initialize a new config object, parsing the input arguments and filling the attributes with their values.
+	 	\param argc the number of input arguments.
+	 	\param argv a pointer to the raw input arguments.
+	 */
 	Config(int argc, char** argv);
 
 public:
 	
-	/// Configuration properties (loaded from command-line or config file).
+	// Configuration properties (loaded from command-line or config file).
+	
+	/// The configuration version number (unused).
 	const size_t version = 1;
 	
+	/// Toggle V-Sync.
 	bool vsync = true;
 	
+	/// Toggle fullscreen window.
 	bool fullscreen = false;
 	
+	/// Toggle log verbose level.
 	bool logVerbose = false;
 	
+	/// Initial width of the window in relative pixels.
 	unsigned int initialWidth = 800;
 	
+	/// Initial height of the window in relative pixels.
 	unsigned int initialHeight = 600;
 	
+	/// \brief Internal vertical rendering resolution.
+	/// \note The width should be computed based on the window aspect ratio.
+	/// \warning The Wwidth should be computed based on the window aspect ratio.
+	/// \todo The TDwidth should be computed based on the window aspect ratio.
+	/// \test The Twidth should be computed based on the window aspect ratio.
+	/// \bug The Bwidth should be computed based on the window aspect ratio.
+	/// \deprecated The Dwidth should be computed based on the window aspect ratio.
+	/// \pre The Prewidth should be computed based on the window aspect ratio.
+	/// \post The Postwidth should be computed based on the window aspect ratio.
+	/// \invariant babdainv
 	float internalVerticalResolution = 720.0f;
 	
-	/// Computed properties.
+	/// Size of the window in raw pixels, updated at launch based on screen density.
 	glm::vec2 screenResolution = glm::vec2(800.0,600.0);
 	
+	/// Screen density, udpated at launch.
 	float screenDensity = 1.0f;
 	
+	/// Output log file path.
 	std::string logPath = "";
 	
 public:
 	
+	/**
+	 Helper to extract (key, [values]) from a configuration file on disk.
+	 \param filePath the path to the configuration file.
+	 \param arguments a dictionary that will be populated with (key, [values]).
+	 */
 	static void parseFromFile(const char* filePath, std::map<std::string, std::vector<std::string>>& arguments);
 	
+	/**
+	 Helper to extract (key, [values]) from the given command-line raw C-style arguments.
+	 \param argc the number of input arguments.
+	 \param argv a pointer to the raw input arguments.
+	 \param arguments a dictionary that will be populated with (key, [values]).
+	 */
 	static void parseFromArgs(const int argc, char** argv, std::map<std::string, std::vector<std::string>> & arguments);
-		
-	void processArguments();
 
 protected:
 	
+	/**
+	 Read the internal (key, [values]) populated dictionary, and transfer their values to the object attributes.
+	 */
+	void processArguments();
+	
+	/// Store the internal parsed (keys, [values]) extracted from a file or the command-line.
 	std::map<std::string, std::vector<std::string>> _rawArguments;
 };
 #endif /* Config_hpp */
