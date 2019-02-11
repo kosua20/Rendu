6be1fde6ebeae3d8fb141721ac91fffbebdddbf9
Simon Rodriguez <kosua20@gmail.com>
2019-02-11 19:26:10 +0100

    Add a basic player object, with a head and tracking segments.
    Former-commit-id: 1e9aa24087bc29a1030bc0e73fe4d867c1fa91cc

---
+++ src/apps/snakegame/Player.cpp
@@ -0,0 +1,153 @@
+#include "Player.hpp"
+#include "input/Input.hpp"
+
+
+Player::Player() {
+	
+	_headProgram = Resources::manager().getProgram("object_basic");
+	_head = Resources::manager().getMesh("head");
+	_bodyElement = Resources::manager().getMesh("body");
+	checkGLError();
+	
+	_path.resize(_numSamplesPath, {glm::vec2(0.0f), 0.0f } );
+}
+
+void Player::update(){
+	if(_currentFrame % _samplingPeriod == 0){
+		// Check if current head position is different from last recorded.
+		const glm::vec2 pos2d(_position);
+		const float distance = glm::distance(pos2d, _path[_currentSample].pos);
+		if(distance > 0.02f){
+			// Move to the next position in the cyclic buffer.
+			_currentSample = (_currentSample + 1) % _numSamplesPath;
+			_path[_currentSample].pos = pos2d;
+			_path[_currentSample].dist = distance;
+		}
+	}
+	// Increase frame count.
+	_currentFrame = (_currentFrame + 1) % _samplingPeriod;
+	
+	if(Input::manager().triggered(Input::KeyQ, true)){
+		_positions.emplace_back(_positions.empty() ? _position : _positions.back());
+	}
+}
+
+void Player::physics(const double frameTime) {
+	
+	
+	
+	const float deltaSpeed = frameTime * _headAccel;
+	const float deltaAngle = frameTime  * _angleSpeed;
+	
+	bool updatedAngle = false;
+	if(Input::manager().pressed(Input::KeyA)){
+		_angle += deltaAngle;
+		updatedAngle = true;
+	}
+	if(Input::manager().pressed(Input::KeyD)){
+		_angle -= deltaAngle;
+		updatedAngle = true;
+	}
+	if(updatedAngle){
+		_momentum[0] = -std::sin(_angle);
+		_momentum[1] =  std::cos(_angle);
+	}
+	
+	
+	const glm::vec3 translation = float(frameTime) * deltaSpeed * _momentum;
+	
+	_position += translation;
+	_position = glm::clamp(_position, -_maxPos, _maxPos);
+	glm::vec2 pos2d(_position);
+	
+	
+	if(!_positions.empty()){
+		int id = 0;
+		float targetDistance = (id+1) * _radius * 2.0f;
+		// Initialize with the segment between the head and the current segment.
+		glm::vec2 nextPoint = _path[_currentSample].pos;
+		glm::vec2 previousPoint = pos2d;
+		// Add an extra shift to add some space while keeping the head centered for collision tests.
+		float newDist = glm::distance(nextPoint, previousPoint) - 0.2f;
+		float totalDistance = newDist;
+		// Then iterate over each sample segment of the path.
+		for(int sid = 0; sid < _numSamplesPath; ++sid){
+			while(totalDistance >= targetDistance && (id < _positions.size())){
+				const float fraction1 = 1.0f-(totalDistance - targetDistance)/(newDist);
+				const glm::vec2 newPos1 = glm::mix(previousPoint, nextPoint, fraction1);
+				_positions[id] = glm::vec3(newPos1, 0.0f);
+				++id;
+				targetDistance = (id+1) * _radius * 2.0f;
+				
+			}
+			if(id >= _positions.size()){
+				break;
+			}
+			// Find the previous point.
+			int pid = _currentSample - sid;
+			if(pid < 0){ pid += _numSamplesPath; }
+			previousPoint = _path[pid].pos;
+			newDist = _path[pid].dist;
+			// Find the next point (the one registered before).
+			int nid = pid - 1;
+			if(nid < 0){ nid += _numSamplesPath; }
+			nextPoint = _path[nid].pos;
+			totalDistance += newDist;
+		}
+		
+	}
+}
+void Player::resize(const glm::vec2 & newRes){
+	//_maxPos[0] = _maxPos[1]*newRes[0]/std::max(1.0f, newRes[1]);
+}
+
+
+
+void Player::draw(const glm::mat4& view, const glm::mat4& projection)  {
+	
+	// Combine the three matrices.
+	// \todo Update model lazily.
+	_model = glm::scale(glm::rotate(glm::translate(glm::mat4(1.0f), _position), _angle, glm::vec3(0.0f, 0.0f, 1.0f)), glm::vec3(_radius));
+	const glm::mat4 VP = projection * view;
+
+	const glm::mat4 MVP = VP * _model;
+	
+	// Compute the normal matrix
+	//glm::mat3 normalMatrix = glm::transpose(glm::inverse(glm::mat3(MV)));
+	// Select the program (and shaders).
+	glUseProgram(_headProgram->id());
+	
+	// Upload the MVP matrix.
+	glUniformMatrix4fv(_headProgram->uniform("mvp"), 1, GL_FALSE, &MVP[0][0]);
+	glBindVertexArray(_head.vId);
+	glDrawElements(GL_TRIANGLES, _head.count, GL_UNSIGNED_INT, (void*)0);
+	
+	for(int i = 0; i < _positions.size();++i){
+		_model = glm::scale(glm::translate(glm::mat4(1.0f), _positions[i]), glm::vec3(_radius));
+		const glm::mat4 MVP1 = VP * _model;
+		glUniformMatrix4fv(_headProgram->uniform("mvp"), 1, GL_FALSE, &MVP1[0][0]);
+		glBindVertexArray(_bodyElement.vId);
+		glDrawElements(GL_TRIANGLES, _bodyElement.count, GL_UNSIGNED_INT, (void*)0);
+	}
+	
+	
+	glBindVertexArray(0);
+	glUseProgram(0);
+}
+
+
+void Player::clean() const {
+	glDeleteVertexArrays(1, &_head.vId);
+	glDeleteVertexArrays(1, &_bodyElement.vId);
+//	for (auto & texture : _textures) {
+//		glDeleteTextures(1, &(texture.id));
+//	}
+}
+//
+//BoundingBox Player::getBoundingBoxWorld() const {
+//	return _mesh.bbox.transformed(_model);
+//}
+//
+//BoundingBox Player::getBoundingBoxModel() const {
+//	return _mesh.bbox;
+//}
---
+++ src/apps/snakegame/Player.hpp
@@ -0,0 +1,51 @@
+#ifndef Player_h
+#define Player_h
+#include "Common.hpp"
+#include "resources/ResourcesManager.hpp"
+
+class Player {
+public:
+	Player();
+	
+	void update();
+	
+	void physics(double frameTime);
+	
+	void draw(const glm::mat4& view, const glm::mat4& projection) ;
+	
+	void clean() const;
+	
+	void resize(const glm::vec2 & newRes);
+	
+private:
+	std::shared_ptr<ProgramInfos> _headProgram;
+	MeshInfos _head;
+	MeshInfos _bodyElement;
+	
+	glm::mat4 _model = glm::mat4(1.0f);
+	
+	glm::vec3 _momentum = glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::vec3 _position = glm::vec3(0.0f);
+	float _angle = 0.0f;
+	
+	std::vector<glm::vec3> _positions;
+	
+	struct PathPoint {
+		glm::vec2 pos;
+		float dist;
+	};
+	std::vector<PathPoint> _path;
+	unsigned int _currentSample = 0;
+	unsigned int _currentFrame = 0;
+	glm::vec3 _maxPos = glm::vec3(8.6f, 5.0f, 0.0f);
+	
+	const size_t _numSamplesPath = 512;
+	const size_t _samplingPeriod = 15;
+	const float _radius = 0.5f;
+	const float _headAccel = 400.0f;
+	const float _angleSpeed = 6.0f;
+	
+	
+};
+
+#endif
