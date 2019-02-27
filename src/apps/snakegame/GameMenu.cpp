52c28c695b99edd4ffc6b2353a1c4e4105751559
Simon Rodriguez <kosua20@gmail.com>
2019-02-28 00:16:52 +0100

    Snake: improve support for buttons and images in menus.
    Support buttons with arbitrary dimensions meshes, same for images.
    
    
    Former-commit-id: 4045beac3012b6767d7961014c87c1a01c25e0e1

--- src/apps/snakegame/GameMenu.cpp
+++ src/apps/snakegame/GameMenu.cpp
@@ -1,18 +1,28 @@
 #include "GameMenu.hpp"
 
 
-MenuButton::MenuButton(const glm::vec2 & apos, const glm::vec2 & asize, const int atag){
-	pos = apos;
-	size = asize;
-	tag = atag;
+MenuButton::MenuButton(const glm::vec2 & screenPos, const glm::vec2 & meshSize, const float screenScale, const int actionTag){
+	pos = screenPos;
+	size = screenScale * meshSize;
+	displayScale = screenScale;
+	tag = actionTag;
 }
 
+MenuImage::MenuImage(const glm::vec2 & screenPos, const float screenScale, const TextureInfos & texture){
+	pos = screenPos;
+	size = screenScale * glm::vec2(1.0f, texture.height / float(texture.width));
+	tid = texture.id;
+}
 
 void GameMenu::update(const glm::vec2 & screenResolution, const float initialRatio){
 	// Update the scaling of each button based on the screen ratio.
 	const float currentRatio = screenResolution[0] / screenResolution[1];
 	const float ratioFix =   initialRatio / currentRatio;
 	for( MenuButton & button : buttons){
-		button.scale = button.size * glm::vec2(ratioFix, 1.0f);
+		button.scale = button.displayScale * glm::vec2(ratioFix, 1.0f);
+	}
+	
+	for( MenuImage & image : images){
+		image.scale = image.size * glm::vec2(ratioFix, 1.0f);
 	}
 }
--- src/apps/snakegame/GameMenu.hpp
+++ src/apps/snakegame/GameMenu.hpp
@@ -1,24 +1,40 @@
 #pragma once
 
-#include "GameMenu.hpp"
 #include "Common.hpp"
+#include "GameMenu.hpp"
+#include "graphics/GLUtilities.hpp"
+
 
 struct MenuButton {
 	
 public:
 	
-	MenuButton(const glm::vec2 & apos, const glm::vec2 & asize, const int atag);
+	MenuButton(const glm::vec2 & screenPos, const glm::vec2 & meshSize, const float screenScale, const int actionTag);
 	
 	enum State {
 		OFF, HOVER, ON
 	};
 	State state = OFF;
 	
-	glm::vec2 pos = glm::vec2(0.0f);
-	glm::vec2 size = glm::vec2(100.0f);
-	glm::vec2 scale = glm::vec2(1.0f);
+	glm::vec2 pos;
+	glm::vec2 size;
+	glm::vec2 scale;
+	float displayScale;
 	
-	int tag = -1;
+	int tag;
+};
+
+struct MenuImage {
+	
+public:
+	
+	MenuImage(const glm::vec2 & screenPos, const float screenScale, const TextureInfos & texture);
+	
+	glm::vec2 pos;
+	glm::vec2 size;
+	glm::vec2 scale;
+	
+	GLuint tid;
 	
 };
 
@@ -30,6 +46,7 @@ public:
 	void update(const glm::vec2 & screenResolution, const float initialRatio);
 	
 	std::vector<MenuButton> buttons;
+	std::vector<MenuImage> images;
 	
-	glm::vec3 backgroundColor = glm::vec3(0.0f);
+	GLuint backgroundImage = 0;
 };
