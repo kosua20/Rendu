c1170de5438552a2170929028949a9a1e2e11609
Simon Rodriguez <kosua20@gmail.com>
2019-07-05 00:13:33 +0200

    ObjToScene: refactor the OBJ loader.
    Former-commit-id: 24c3193ec06ac39c1cc03583619808c2e2bee93b

---
+++ src/tools/objtoscene/CompositeObj.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "Common.hpp"
+#include "helpers/TextUtilities.hpp"
+#include "resources/MeshUtilities.hpp"
+
+/**
+ \brief Commposite OBJ files loading.
+ \ingroup ObjToScene
+ */
+namespace CompositeObj {
+
+	/**
+	 \brief OBJ material descriptor.
+	 */
+	struct Material {
+		// Textures.
+		std::string normalTexturePath;
+		std::string alphaTexturePath;
+		std::string displacementTexturePath;
+		std::string colorTexturePath;
+		std::string roughTexturePath;
+		std::string metalTexturePath;
+		std::string specTexturePath;
+		
+		// Scalars.
+		glm::vec3 color = glm::vec3(0.0f);
+		float rough = 0.0f;
+		float metal = 0.0f;
+		float spec  = 0.0f;
+		
+		// Have some of the scalars been set?
+		bool hasColor = false;
+		bool hasRough = false;
+		bool hasMetal = false;
+		bool hasSpec = false;
+	};
+
+	/**
+	 \brief Associate a mesh and a material.
+	 */
+	struct Object {
+		
+		Mesh mesh; ///< The mesh.
+		std::string name; ///< Name of the object.
+		std::string material; ///< Name of the material.
+		
+		/**
+		 Constructor.
+		 \param aName the name of the object.
+		 */
+		Object(const std::string & aName){
+			name = aName;
+		}
+	};
+
+	/** Load a multi-objects, multi-materials OBJ file.
+	 \param filePath the path to the OBJ file
+	 \param objects will be filled the objects infos
+	 \param materials will be filled with the materials infos
+	 \return an error code or 0
+	 */
+	int load(const std::string & filePath, std::vector<Object>& objects, std::map<std::string, Material>& materials);
+
+}
