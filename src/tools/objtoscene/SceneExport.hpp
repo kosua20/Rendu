ace34cf6998d35ae1c0a685985fe7dc051820f23
Simon Rodriguez <kosua20@gmail.com>
2019-07-05 00:13:52 +0200

    ObjToScene: refactor the scene export utilities.
    Former-commit-id: aa76652893eb4358ca0e200cc7850775a08c855d

---
+++ src/tools/objtoscene/SceneExport.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "Common.hpp"
+#include "resources/ImageUtilities.hpp"
+#include "resources/MeshUtilities.hpp"
+#include "helpers/Interface.hpp"
+#include "helpers/TextUtilities.hpp"
+#include "CompositeObj.hpp"
+
+/**
+ \brief Helpers to export a Rendu scene.
+ \ingroup ObjToScene
+ */
+namespace SceneExport {
+	
+	/** \brief Contain exported texture infos for a given material.
+	 */
+	struct Material {
+		std::string colorName; ///< Color texture name.
+		std::string normalName; ///< Normal map name.
+		std::string roughMetAoName; ///< Roughness-metalness-ambient occlusion texture name.
+		std::string depthName; ///< Optional depth map.
+	};
+	
+	/** Save a material parameters as a series of textures.
+	 \param baseName material base name
+	 \param material the material to export
+	 \param outputDirPath the output directory
+	 \return the exported material information
+	 */
+	Material saveMaterial(const std::string & baseName, const CompositeObj::Material & material, const std::string & outputDirPath);
+
+	/** Save a scene description, listing all objects with materials. The file can then be decoded by Codable objects.
+	 \param objects the scene objects
+	 \param materials the scene materials
+	 \param outputFile the destination file
+	 \return an error code or 0
+	 */
+	int saveDescription(const std::vector<CompositeObj::Object> & objects, const std::map<std::string, Material> & materials, const std::string & outputFile);
+	
+}
