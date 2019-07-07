54bdd42c109f7be3269843a8725313063f4191c7
Simon Rodriguez <kosua20@gmail.com>
2019-07-07 09:40:21 +0200

    System: add a parallel for helper.
    Internally spawn threads that each get a subrange of the total work range.
    
    
    Former-commit-id: 446c01b06d5231ac7f204aed9ca08ce39263613c

--- src/engine/helpers/System.hpp
+++ src/engine/helpers/System.hpp
@@ -5,6 +5,7 @@
 
 #include <imgui/imgui.h>
 #include <imgui/imgui_impl_glfw.h>
+#include <thread>
 
 struct GLFWwindow;
 
@@ -77,6 +78,48 @@ namespace System {
 	 \warning This function will not create intermediate directories.
 	 */
 	bool createDirectory(const std::string & directory);
+	
+	
+	/** Multi-threaded for-loop.
+	 \param low lower (included) bound
+	 \param high higher (excluded) bound
+	 \param func the function to execute at each iteration, will receive the index of the
+	 element as a unique argument. Signature: void func(size_t i)
+	 \note For now only an increment by one is supported.
+	 */
+	template<typename ThreadFunc>
+	void forParallel(size_t low, size_t high, ThreadFunc func){
+		// Make sure the loop is increasing.
+		if(high < low){
+			const size_t temp = low;
+			low = high;
+			high = temp;
+		}
+		// Prepare the threads pool.
+		const size_t count = size_t(std::max(std::thread::hardware_concurrency(), unsigned(1)));
+		std::vector<std::thread> threads;
+		threads.reserve(count);
+		
+		// Compute the span of each thread.
+		size_t span = size_t(std::round((float(high) - float(low))/float(count)));
+		span = std::max(size_t(1), span);
+		
+		// Helper to execute the function passed on a subset of the total interval.
+		auto launchThread = [&func](size_t a, size_t b) {
+			for(size_t i = a; i < b; ++i) {
+				func(i);
+			}
+		};
+		
+		for(size_t tid = 0; tid < count; ++tid){
+			// For each thread, call the same lambda with different bounds as arguments.
+			const size_t threadLow = tid * span;
+			size_t threadHigh = (tid == count - 1) ? high : ((tid+1) * span);
+			threads.emplace_back(launchThread, threadLow, threadHigh);
+		}
+		// Wait for all threads to finish.
+		std::for_each(threads.begin(),threads.end(),[](std::thread& x){x.join();});
+	}
 	
 }
 
