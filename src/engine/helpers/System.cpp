64a17ec8cea5b2c3979b7c51ffeded78ac88225b
Simon Rodriguez <kosua20@gmail.com>
2019-07-08 23:34:13 +0200

    GUI: update style.
    Former-commit-id: 2677cd73a1abf6e6dcfe63ae40020c4ff21e2794

--- src/engine/helpers/System.cpp
+++ src/engine/helpers/System.cpp
@@ -18,7 +18,50 @@ namespace System {
 			ImGuiIO& io = ImGui::GetIO(); (void)io;
 			ImGui_ImplGlfw_InitForOpenGL(window, false);
 			ImGui_ImplOpenGL3_Init("#version 150");
+			
+			// Customize the style.
 			ImGui::StyleColorsDark();
+			ImGuiStyle& style = ImGui::GetStyle();
+			// Colors.
+			ImVec4* colors = style.Colors;
+			colors[ImGuiCol_WindowBg]               = ImVec4(0.04f, 0.04f, 0.04f, 0.94f);
+			colors[ImGuiCol_FrameBg]                = ImVec4(0.16f, 0.16f, 0.16f, 0.54f);
+			colors[ImGuiCol_FrameBgHovered]         = ImVec4(0.36f, 0.36f, 0.36f, 0.40f);
+			colors[ImGuiCol_FrameBgActive]          = ImVec4(0.54f, 0.54f, 0.54f, 0.67f);
+			colors[ImGuiCol_TitleBgActive]          = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+			colors[ImGuiCol_ScrollbarGrab]          = ImVec4(0.17f, 0.17f, 0.17f, 1.00f);
+			colors[ImGuiCol_ScrollbarGrabHovered]   = ImVec4(0.34f, 0.34f, 0.34f, 1.00f);
+			colors[ImGuiCol_ScrollbarGrabActive]    = ImVec4(0.41f, 0.41f, 0.41f, 1.00f);
+			colors[ImGuiCol_CheckMark]              = ImVec4(0.84f, 0.84f, 0.84f, 1.00f);
+			colors[ImGuiCol_SliderGrab]             = ImVec4(0.30f, 0.30f, 0.30f, 1.00f);
+			colors[ImGuiCol_SliderGrabActive]       = ImVec4(0.64f, 0.64f, 0.64f, 1.00f);
+			colors[ImGuiCol_Button]                 = ImVec4(0.68f, 0.68f, 0.68f, 0.40f);
+			colors[ImGuiCol_ButtonHovered]          = ImVec4(0.40f, 0.40f, 0.40f, 1.00f);
+			colors[ImGuiCol_ButtonActive]           = ImVec4(0.53f, 0.53f, 0.53f, 1.00f);
+			colors[ImGuiCol_Header]                 = ImVec4(0.57f, 0.57f, 0.57f, 0.31f);
+			colors[ImGuiCol_HeaderHovered]          = ImVec4(0.49f, 0.49f, 0.49f, 0.80f);
+			colors[ImGuiCol_HeaderActive]           = ImVec4(0.52f, 0.52f, 0.52f, 1.00f);
+			colors[ImGuiCol_Separator]              = ImVec4(0.41f, 0.41f, 0.41f, 0.50f);
+			colors[ImGuiCol_SeparatorHovered]       = ImVec4(0.43f, 0.43f, 0.43f, 0.78f);
+			colors[ImGuiCol_SeparatorActive]        = ImVec4(0.44f, 0.44f, 0.44f, 1.00f);
+			colors[ImGuiCol_ResizeGrip]             = ImVec4(1.00f, 1.00f, 1.00f, 0.25f);
+			colors[ImGuiCol_ResizeGripHovered]      = ImVec4(0.84f, 0.84f, 0.84f, 0.67f);
+			colors[ImGuiCol_ResizeGripActive]       = ImVec4(0.88f, 0.88f, 0.88f, 0.95f);
+			colors[ImGuiCol_PlotLinesHovered]       = ImVec4(0.96f, 0.96f, 0.96f, 1.00f);
+			colors[ImGuiCol_PlotHistogram]          = ImVec4(0.62f, 0.62f, 0.62f, 1.00f);
+			colors[ImGuiCol_PlotHistogramHovered]   = ImVec4(0.90f, 0.90f, 0.90f, 1.00f);
+			colors[ImGuiCol_TextSelectedBg]         = ImVec4(0.67f, 0.67f, 0.67f, 0.35f);
+			colors[ImGuiCol_DragDropTarget]         = ImVec4(0.83f, 0.83f, 0.83f, 0.90f);
+			colors[ImGuiCol_NavHighlight]           = ImVec4(0.88f, 0.88f, 0.88f, 1.00f);
+			// Frames.
+			style.FrameRounding = 5.0f;
+			style.GrabRounding = 3.0f;
+			style.WindowRounding = 5.0f;
+			style.ScrollbarRounding = 12.0f;
+			style.ScrollbarSize = 12.0f;
+			style.WindowTitleAlign.x = 0.5f;
+			style.FramePadding.y = 4.0f;
+			style.ItemSpacing.y = 3.0f;
 		}
 
 		void beginFrame() {
