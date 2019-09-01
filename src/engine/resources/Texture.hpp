30d83f8fd109cf5eae8c54679e223127886f4393
Simon Rodriguez <kosua20@gmail.com>
2019-09-01 12:12:08 +0200

    Texture: cubemap sampling, cleanup.

--- src/engine/resources/Texture.hpp
+++ src/engine/resources/Texture.hpp
@@ -18,11 +18,9 @@ inline TextureShape & operator|=(TextureShape t0, TextureShape t1) {
  */
 class Texture {
 public:
-	/** Clear CPU images data. */
-	void clearImages();
-
-	/** Cleanup all data. */
-	void clean();
+	
+	/** Constructor. */
+	Texture() = default;
 
 	/** Send to the GPU.
 	 \param layout the data layout and type to use for the texture
@@ -30,13 +28,41 @@ public:
 	 */
 	void upload(const Descriptor & layout, bool updateMipmaps);
 
+	/** Clear CPU images data. */
+	void clearImages();
+	
+	/** Cleanup all data. */
+	void clean();
+		
+	/** Bilinearly sample a cubemap in a given direction.
+	 \param dir the direction to sample
+	 \return the sampled color.
+	 */
+	glm::vec3 sampleCubemap(const glm::vec3 & dir) const;
+	
+	/** Copy assignment operator (disabled).
+	 \return a reference to the object assigned to
+	 */
+	Texture & operator=(const Texture &) = delete;
+	
+	/** Copy constructor (disabled). */
+	Texture(const Texture &) = delete;
+	
+	/** Move assignment operator .
+	 \return a reference to the object assigned to
+	 */
+	Texture & operator=(Texture &&) = default;
+	
+	/** Move constructor. */
+	Texture(Texture &&) = default;
+	
 	std::vector<Image> images;		 ///< The images CPU data (optional).
 	std::unique_ptr<GPUTexture> gpu; ///< The GPU data (optional).
-
+	
 	unsigned int width  = 0; ///< The texture width.
 	unsigned int height = 0; ///< The texture height.
 	unsigned int depth  = 1; ///< The texture depth.
 	unsigned int levels = 1; ///< The mipmap count.
-
+	
 	TextureShape shape = TextureShape::D2; ///< Texure type.
 };
