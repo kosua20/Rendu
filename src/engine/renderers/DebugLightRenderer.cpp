54d43fff0a2a64843e1de220e846cc3865e6594e
Simon Rodriguez <kosua20@gmail.com>
2019-11-12 23:11:34 +0100

    Lights: add a debug renderer.
    Visualize each type of lights as a different wireframe object.

---
+++ src/engine/renderers/DebugLightRenderer.cpp
@@ -0,0 +1,53 @@
+#include "DebugLightRenderer.hpp"
+#include "graphics/GLUtilities.hpp"
+
+DebugLightRenderer::DebugLightRenderer(const std::string & fragmentShader){
+	_sphere  = Resources::manager().getMesh("light_sphere", Storage::GPU);
+	_cone  = Resources::manager().getMesh("light_cone", Storage::GPU);
+	_arrow  = Resources::manager().getMesh("light_arrow", Storage::GPU);
+	_program = Resources::manager().getProgram(fragmentShader, "object_basic", fragmentShader);
+}
+
+void DebugLightRenderer::updateCameraInfos(const glm::mat4 & viewMatrix, const glm::mat4 & projMatrix){
+	_view = viewMatrix;
+	_proj = projMatrix;
+}
+
+void DebugLightRenderer::draw(const SpotLight * light) const {
+	const glm::mat4 mvp		 = _proj * _view * light->model();
+	const glm::vec3 & color = light->intensity();
+	const glm::vec3 colorLow = color / std::max(color[0], std::max(color[1], color[2]));
+	
+	_program->use();
+	_program->uniform("mvp", mvp);
+	_program->uniform("color", glm::vec4(colorLow, 1.0f));
+	GLUtilities::drawMesh(*_cone);
+}
+
+void DebugLightRenderer::draw(const PointLight * light) const {
+	const glm::mat4 mvp  = _proj * _view * light->model();
+	const glm::mat4 mvp1 = glm::scale(mvp , glm::vec3(0.02f));
+	
+	const glm::vec3 & color  = light->intensity();
+	const glm::vec3 colorLow = color / std::max(color[0], std::max(color[1], color[2]));
+	
+	_program->use();
+	_program->uniform("mvp", mvp);
+	_program->uniform("color", glm::vec4(colorLow, 1.0f));
+	GLUtilities::drawMesh(*_sphere);
+	_program->uniform("mvp", mvp1);
+	_program->uniform("color", glm::vec4(color, 1.0f));
+	GLUtilities::drawMesh(*_sphere);
+}
+
+void DebugLightRenderer::draw(const DirectionalLight * light) const {
+	const glm::mat4 vp		 = _proj * _view * light->model();
+	const glm::vec3 & color = light->intensity();
+	const glm::vec3 colorLow = color / std::max(color[0], std::max(color[1], color[2]));
+	
+	_program->use();
+	_program->uniform("mvp", vp);
+	_program->uniform("color", glm::vec4(colorLow, 1.0f));
+	GLUtilities::drawMesh(*_arrow);
+	
+}
---
+++ src/engine/renderers/DebugLightRenderer.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "scene/Scene.hpp"
+
+#include "renderers/LightRenderer.hpp"
+#include "scene/lights/Light.hpp"
+#include "scene/lights/PointLight.hpp"
+#include "scene/lights/DirectionalLight.hpp"
+#include "scene/lights/SpotLight.hpp"
+
+#include "Common.hpp"
+
+/**
+ \brief Visualize lights as colored wireframe objects.
+ \ingroup Engine
+ */
+class DebugLightRenderer final : public LightRenderer {
+
+public:
+	/** Constructor. \see GLSL::Frag::light_debug for an example.
+	 \param fragmentShader the name of the fragment shader to use.
+	 */
+	explicit DebugLightRenderer(const std::string & fragmentShader);
+	
+	/** Set the current user view and projection matrices.
+	 \param viewMatrix the camera view matrix
+	 \param projMatrix the camera projection matrix
+	 */
+	void updateCameraInfos(const glm::mat4 & viewMatrix, const glm::mat4 & projMatrix);
+	
+	/** Draw a spot light as a colored wireframe cone.
+	 \param light the light to draw
+	 */
+	void draw(const SpotLight * light) const override;
+	
+	/** Draw a point light as a colored wireframe sphere.
+	 \param light the light to draw
+	 */
+	void draw(const PointLight * light) const override;
+	
+	/** Draw a directional light as a colored wireframe arrow pointing at the origin.
+	 \param light the light to draw
+	 */
+	void draw(const DirectionalLight * light) const override;
+
+private:
+	
+	const Mesh * _sphere; ///< Point light supporting geometry.
+	const Mesh * _cone;   ///< Spot light supporting geometry.
+	const Mesh * _arrow;  ///< Spot light supporting geometry.
+	
+	const Program * _program; ///< Light mesh shader.
+	
+	glm::mat4 _view = glm::mat4(1.0f); ///< Cached camera view matrix.
+	glm::mat4 _proj = glm::mat4(1.0f); ///< Cached camera projection matrix.
+};
