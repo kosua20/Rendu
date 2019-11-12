0f6754f2d98dd0885b068ad2f37bd0ed69cfbcf1
Simon Rodriguez <kosua20@gmail.com>
2019-11-12 23:11:11 +0100

    Lights: implement a renderer interface.
    Each type of light is handled separately.

---
+++ src/engine/renderers/LightRenderer.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "system/Config.hpp"
+
+class DirectionalLight;
+class PointLight;
+class SpotLight;
+
+/**
+ \brief Base structure of a per-light specialized renderer. It can be applied to a light by calling light->draw(renderer), which will then call the corresponding method below.
+ \ingroup Engine
+ */
+class LightRenderer {
+
+public:
+	
+	/** Perform a rendering task for a directional light.
+	 \param light the light to visit.
+	 */
+	virtual void draw(const DirectionalLight * light) const = 0;
+	
+	/** Perform a rendering task for a point light.
+	 \param light the light to visit.
+	 */
+	virtual void draw(const PointLight * light) const = 0;
+	
+	/** Perform a rendering task for a spot light.
+	 \param light the light to visit.
+	 */
+	virtual void draw(const SpotLight * light) const = 0;
+	
+	
+};
