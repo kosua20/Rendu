a86c4b74f536e043452986800d38f627a88d3827
Simon Rodriguez <kosua20@gmail.com>
2019-12-14 15:05:55 +0100

    Documentation: improve support for shader interface blocks members.

--- docs/theme/process-glsl.py
+++ docs/theme/process-glsl.py
@@ -29,20 +29,38 @@ doxyGroup = "Shaders"
 doxySubnamespace = fileExt.lower()[1:].capitalize()
 doxyClass = fileName.capitalize().replace("-", "_")
 doxyFullNames = {"Vert" : "vertex", "Frag" : "fragment", "Geom" : "geometry" }
+structAddKeyword = {"Vert" : "out", "Frag" : "in", "Geom" : "in" }
 doxyClassDetail = doxyClass.replace("_", " ")
+structKeyword = structAddKeyword[doxySubnamespace]
 # C++ namespaces
 printout("namespace " + doxyNamespace + "{\n")
 printout("namespace " + doxySubnamespace + "{\n")
 # Class
 printout("/** \\class " + doxyClass + "\n")
 printout("  * \\brief \"" + doxyClassDetail + "\" " + doxyFullNames[doxySubnamespace] + " shader." + "\n")
-printout("  * \\ingroup " + doxyGroup + "*/\n")
+printout("  * \\ingroup " + doxyGroup + "\n*/\n")
 printout("public class " + doxyClass + " {" + "\n")
 printout("public:" + "\n")
+
+inInterfaceBlock = False
 # Content of the shader
 for line in fileLines:
-	printout(line)
+	printLine = True
+	# Detect layout keyword at beginning and remove layout block that make Doxygen think it's a function.
+	if line.lstrip().startswith("layout"):
+		line = line[line.find(")")+1:].lstrip()
+	if line.find("INTERFACE")>=0:
+		printLine = False
+		inInterfaceBlock = True
+	if inInterfaceBlock and line.find("}") >= 0:
+		printLine = False
+		inInterfaceBlock = False
+	if inInterfaceBlock and len(line) > 0:
+		line = structKeyword + " " + line.lstrip()
+	
+	if printLine:
+		printout(line)
 # Close the class, the namespaces, the group.
-printout("}" + "\n}\n}\n")
+printout("\n}}}\n")
 
 sys.exit(0)
