6cc1c39ab4ee5715b00df3ac684810818ac95795
Simon Rodriguez <kosua20@gmail.com>
2021-02-15 01:02:59 +0100

    ImGui: disable texture display.

--- src/engine/resources/Texture.cpp
+++ src/engine/resources/Texture.cpp
@@ -105,9 +105,10 @@ const std::string & Texture::name() const {
 }
 
 void ImGui::Image(const Texture & texture, const ImVec2& size, const ImVec2& uv0, const ImVec2& uv1, const ImVec4& tint_col, const ImVec4& border_col){
-	ImGui::Image(reinterpret_cast<void *>(static_cast<uintptr_t>(texture.gpu->id)), size, uv0, uv1, tint_col, border_col);
+	//ImGui::Image(reinterpret_cast<void *>(static_cast<uintptr_t>(texture.gpu->id)), size, uv0, uv1, tint_col, border_col);
 }
 
 bool ImGui::ImageButton(const Texture & texture, const ImVec2& size, const ImVec2& uv0,  const ImVec2& uv1, int frame_padding, const ImVec4& bg_col, const ImVec4& tint_col){
-	return ImGui::ImageButton(reinterpret_cast<void *>(static_cast<uintptr_t>(texture.gpu->id)), size, uv0, uv1, frame_padding, bg_col, tint_col);
+	return false;
+	//ImGui::ImageButton(reinterpret_cast<void *>(static_cast<uintptr_t>(texture.gpu->id)), size, uv0, uv1, frame_padding, bg_col, tint_col);
 }
