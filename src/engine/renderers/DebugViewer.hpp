009dd2266f4f59f35cc05a1354a4da935267347e
Simon Rodriguez <kosua20@gmail.com>
2021-08-06 23:08:46 +0200

    DebugViewer: use default textures.

--- src/engine/renderers/DebugViewer.hpp
+++ src/engine/renderers/DebugViewer.hpp
@@ -59,7 +59,7 @@ public:
 	void interface();
 
 	/** Destructor */
-	~DebugViewer();
+	~DebugViewer() = default;
 
 	/** Copy constructor.*/
 	DebugViewer(const DebugViewer &) = delete;
@@ -197,8 +197,6 @@ private:
 	std::vector<FramebufferInfos> _framebuffers; ///< The registered framebuffers.
 	std::vector<MeshInfos> _meshes; ///< The registered meshes.
 	std::map<std::string, StateInfos> _states; ///< GPU states currently tracked.
-
-	std::unordered_map<TextureShape, Texture> _defaultTextures;
 
 	Program * _texDisplay; ///< Texture display shader.
 	const bool _silent; ///< Don't register or display anything.
